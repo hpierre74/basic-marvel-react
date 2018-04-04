16db324c0468ea6feb55faa96a2bd199ef277108
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2018-04-04 13:01:47 +0200

    rm uncomplete apps until complete

--- SSR/src/server/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    marvel_public: process.env.RAZZLE_MARVEL_PUBLIC,
-    marvel_private: process.env.RAZZLE_MARVEL_PRIVATE,    
-    mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/marvel-api',
-    port: process.env.PORT || 3000
-}
-  
-export default config;
